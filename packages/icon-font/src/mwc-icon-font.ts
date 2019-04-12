d156a039f8fbe124830853f658659a51e14f230e
straversi <straversi@google.com>
2019-04-12 15:38:50 -0700

    concept

---
+++ packages/icon-font/src/mwc-icon-font.ts
@@ -0,0 +1,6 @@
+console.log("heyoooo");
+
+const fontEl = document.createElement('link');
+fontEl.rel = 'stylesheet';
+fontEl.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+document.head!.appendChild(fontEl);
